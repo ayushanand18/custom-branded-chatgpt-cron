0d1775bb5256deb60d0824817b08a0a3ab15f3c9
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-06-07 04:17:51 +0000

    setting up cron job

---
+++ netlify/functions/ping-api.js
@@ -0,0 +1,14 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/test-scheduled-function.js
+
+const { schedule } = require("@netlify/functions");
+
+const handler = async function(event, context) {
+    await fetch('https://custom-branded-chatgpt-api.onrender.com/test')
+    console.log('Received event:', event);
+
+    return {
+        statusCode: 200,
+    };
+};
+
+exports.handler = schedule("@hourly", handler);
